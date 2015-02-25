cbe45d78eec309c39cc9c4d937d214a3d7ee10c7
Drew Hudec <hudec.da@gmail.com>
2015-02-24 19:26:06 -0500

    Added ability to switch between prod/dev environments; moved mongo user and pw to env variables

--- config/db.js
+++ config/db.js
@@ -1,3 +1,19 @@
-module.exports = {
-	url: 'mongodb://localhost/feedme-dev'
-}
+var development = (function() {
+	var config = {};
+
+	config.url = 'mongodb://localhost/feedme-dev'
+
+	return config;
+}());
+
+var production = (function() {
+	var config = {};
+	var dbUser = process.env.MONGO_USER || '';
+	var dbPw = process.env.MONGO_PW || '';
+
+	config.url = 'mongodb://' + dbUser +':' + dbPw + '@ds047901.mongolab.com:47901/feedme'
+
+	return config;
+}());
+
+module.exports = (process.env.MODE == 'production') ? production : development;
