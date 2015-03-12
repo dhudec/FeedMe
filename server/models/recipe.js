91ccd01c6bb4eed48025090efdfe501f29cc3375
Drew Hudec <hudec.da@gmail.com>
2015-03-11 22:41:17 -0400

    adding can unit. added ingredient special notes.

--- server/models/recipe.js
+++ server/models/recipe.js
@@ -7,7 +7,7 @@ var RecipeSchema = new mongoose.Schema({
 	prepTime: {type: Number},
 	cookTime: {type: Number},
 	instructions: {type: String},
-	ingredients: [ { quantity: {type: Number}, units: {type: String, enum: Units.ingredientUnits}, item: {type: mongoose.Schema.ObjectId, ref: 'Ingredient'} } ],
+	ingredients: [ { quantity: {type: Number}, units: {type: String, enum: Units.ingredientUnits}, item: {type: mongoose.Schema.ObjectId, ref: 'Ingredient'}, note: {type: String} } ],
     categories: [ {type: mongoose.Schema.ObjectId, ref: 'RecipeCategory'} ],
     image: {type: String}
 });
--- server/models/units.js
+++ server/models/units.js
@@ -1,3 +1,3 @@
-var ingredientUnits = 'lb; oz; c; qt; gal; tsp; tbsp; dash; pinch; to taste'.split('; ');
+var ingredientUnits = 'lb; oz; c; qt; gal; tsp; tbsp; dash; pinch; to taste; can'.split('; ');
 
 module.exports.ingredientUnits = ingredientUnits;
